6851c12a5cd264e90aa9733015ecca96be214d29
Gabriel Soricelli <sorigab@sjb-liege.org>
2025-04-14 14:37:29 +0200

    main

--- unite.py
+++ unite.py
@@ -1,9 +1,45 @@
 """objet unite"""
 
-from plateau import Cellule
+import random
 
+from plateau import Cellule
+import config.py
+
+dict_usa = {
+    0 : ["M24"],
+    1 : ["M4A2"],
+    2 : ["T1E1"],
+    3 : ["M10 GMC"],
+}
+
+dict_germany = {
+    0 : ["Puma"],
+    1 : ["Panther"],
+    2 : ["Tiger"],
+    3 : ["Maus"],
+    4 : ["Leopard"],
+    
+    
+}
+    
+
+dict_nation = {
+    "USA": dict_usa,
+    "Germany": 1,
+    "URSS": 2
+}
 
 class Unite():
-    
-    
-class Nation():
+    def __init__(self, infolist: list[str]):
+        
+        self.name = infolist[0]
+        self.classe = infolist[1]
+        
+        self.max_health = int(infolist[2])
+        self.health = self.max_health
+        self.armor = int(infolist[3])
+        
+        self.speed = int(infolist[4])
+        self.range = int(infolist[5])
+        
+        
